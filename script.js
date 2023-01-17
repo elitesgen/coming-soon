981f18acf4b44848e322667b7114426d0ef54f93
O.Remilekun <remmylknolaotan@gmail.com>
2023-01-17 12:32:27 +0100

    Improve carousel

--- script.js
+++ script.js
@@ -18,5 +18,36 @@ const swiper = new Swiper('.swiper', {
     scrollbar: {
       el: '.swiper-scrollbar',
     },
+
+    // Breakpoints
+    // slidesPerView: 1,
+    // spaceBetween: 10,
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 20
+        },
+        // when window width is >= 480px
+        480: {
+          slidesPerView: 3,
+          spaceBetween: 30
+        },
+        // when window width is >= 640px
+        640: {
+          slidesPerView: 4,
+          spaceBetween: 40
+        }
+      },
+
+    //   cssMode: true,
+    //   height: 100,
+      centeredSlides: true,
+    //   centeredSlidesBounds: true,
+
   });
+
+
+//   containerModifierClass	string	'swiper-'	
+// The beginning of the modifier CSS class that can be added to swiper container depending on different parameters
   
